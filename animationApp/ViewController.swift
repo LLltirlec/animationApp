296e0cec393dd85f251adbd786e60a9ce5da18ff
LLltirlec <fhntvrf1998@gmail.com>
2023-06-14 03:09:03 +0300

    Initial Commit

---
+++ animationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  animationApp
+//
+//  Created by Евгений Ефимов on 14.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
